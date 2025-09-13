81dcf67a9203858c8bcb3c74b1a6d3cad56e2e3d
Caeson Reckling <pec22015@byui.edu>
2025-09-12 19:41:17 -0600

    Update for newer library
    Updated the code to work with a newer library. Hardware still not working.

--- LGTVRemote/LGTVRemote.ino
+++ LGTVRemote/LGTVRemote.ino
@@ -14,18 +14,22 @@
 #define BACK '#'
 #define INPUT 'D'
 
-// LG Code Macros:
-#define SELECT_CODE      0x44000000
-#define UP_CODE          0x40000000
-#define DOWN_CODE        0x41000000
-#define LEFT_CODE        0x07000000
-#define RIGHT_CODE       0x06000000   
-#define VOLUME_UP_CODE   0x02000000
-#define VOLUME_DOWN_CODE 0x03000000
-#define POWER_CODE       0x08000000
-#define HOME_CODE        0x7C000000
-#define BACK_CODE        0x28000000
-#define INPUT_CODE       0x0B000000
+// NEC commands are send with a 2 byte address and 2 byte command
+// LG Command MACROS:
+#define SELECT_CODE      0x22DD
+#define UP_CODE          0x02FD
+#define DOWN_CODE        0x827D
+#define LEFT_CODE        0xE01F
+#define RIGHT_CODE       0x609F
+#define VOLUME_UP_CODE   0x40BF
+#define VOLUME_DOWN_CODE 0xC03F
+#define POWER_CODE       0x10EF
+#define HOME_CODE        0xA25D
+#define BACK_CODE        0x14EB
+#define INPUT_CODE       0xD02F
+
+// Address Macro
+#define NEC_ADDRESS 0x20DF
 
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // Four columns
@@ -47,8 +51,6 @@ byte colPins[COLS] = {6, 7, 8, 9}; // Connect to the column pinouts of the keypa
 // Create a Keypad object
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-// IRsend object
-IRsend irsend(10);
 
 // Debug Print Model
 void printKey(char keyPress) {
@@ -105,7 +107,9 @@ void printKey(char keyPress) {
   }
 }
 
-void sendKey(char keyPress, IRsend irsend) {
+// Sends commands
+// IrSender.sendNEC(address, command, number of repeats)
+void sendKey(char keyPress) {
   Serial.print("Sending Key: ");
   switch(keyPress) {
     case '1':                         // Row One
@@ -113,37 +117,37 @@ void sendKey(char keyPress, IRsend irsend) {
       break;
     case UP:
       Serial.println(UP_CODE, HEX);
-      irsend.sendNEC(UP_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, UP_CODE, 0);
       break;
     case '3':
       Serial.println("No Value");
       break;
     case VOLUME_UP:
       Serial.println(VOLUME_UP_CODE, HEX);
-      irsend.sendNEC(VOLUME_UP_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, VOLUME_UP_CODE, 0);
       break;
     case LEFT:                      // Row Two
       Serial.println(LEFT_CODE, HEX);
-      irsend.sendNEC(LEFT_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, LEFT_CODE, 0);
       break;
     case SELECT:
       Serial.println(SELECT_CODE, HEX);
-      irsend.sendNEC(SELECT_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, SELECT_CODE, 0);
       break;
     case RIGHT:
       Serial.println(RIGHT_CODE, HEX);
-      irsend.sendNEC(RIGHT_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, RIGHT_CODE, 0);
       break;
     case VOLUME_DOWN:
       Serial.println(VOLUME_DOWN_CODE, HEX);
-      irsend.sendNEC(VOLUME_DOWN_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, VOLUME_DOWN_CODE, 0);
       break;
     case '7':                      // Row Three
       Serial.println("No Value");
       break;
     case DOWN:
       Serial.println(DOWN_CODE, HEX);
-      irsend.sendNEC(DOWN_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, DOWN_CODE, 0);
       break;
     case '9':
       Serial.println("No Value");
@@ -153,25 +157,26 @@ void sendKey(char keyPress, IRsend irsend) {
       break;
     case HOME:                      // Row Four
       Serial.println(HOME_CODE, HEX);
-      irsend.sendNEC(HOME_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, HOME_CODE, 0);
       break;
     case POWER:
       Serial.println(POWER_CODE, HEX);
-      irsend.sendNEC(POWER_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, POWER_CODE, 0);
       break;
     case BACK:
       Serial.println(BACK_CODE, HEX);
-      irsend.sendNEC(BACK_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, BACK_CODE, 0);
       break;
     case INPUT:
       Serial.println(INPUT_CODE, HEX);
-      irsend.sendNEC(INPUT_CODE, 32);
+      IrSender.sendNEC(NEC_ADDRESS, INPUT_CODE, 0);
       break;
   }
 }
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication at 9600 baud
+  IrSender.begin(10); // Data Line is attached to pin 10
   Serial.println("LG TV Remote Test");
 }
 
@@ -180,7 +185,7 @@ void loop() {
 
   if(key) {
     printKey(key); // Print key pressed
-    sendKey(key, irsend); // Send KeyCode 
+    sendKey(key); // Send KeyCode 
     Serial.print("\n");
   }
 }
